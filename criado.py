05d258a74ac6003662492019ef9d53abf2b18e08
62random <josepedropinto98@gmail.com>
2021-02-24 02:07:11 +0000

    schedule

--- criado.py
+++ criado.py
@@ -1,3 +1,4 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
 import pandas as pd
 import lxml.html
 import requests
@@ -19,10 +20,9 @@ SMTP_SERVER = "smtp.gmail.com"
 
 # Read wishlist and database
 wishlist = [x for x in open(WISHLIST_FILE, "r").readlines()]
+df = pd.DataFrame(columns=['item', 'url', 'title', 'price'])
 if os.path.exists(DATA_FILE):
     df = pd.read_csv(DATA_FILE)
-else:
-    df = pd.read_csv(f"{DATA_FILE}.sample")
 
 df = df[df['item'].isin(wishlist)]
 
@@ -34,21 +34,21 @@ results = {
 }
 
 
-def save_ad(results, item, url, title, price):
-    results['item'].append(item)
-    results['url'].append(url)
-    results['title'].append(title)
-    results['price'].append(price)
+def save_ad(r, item, url, title, price):
+    r['item'].append(item)
+    r['url'].append(url)
+    r['title'].append(title)
+    r['price'].append(price)
 
 
-def email_results(results):
-    if len(results['url']) == 0:
+def email_results(r):
+    if len(r['url']) == 0:
         return
     message = "Novos anúncios encontrados!"
-    for i in range(len(results['url'])):
-        message += f"\nItem: {results['item'][i]}"
-        message += f"\nPreço: {results['price'][i]}"
-        message += f"\nUrl: {results['url'][i]}"
+    for i in range(len(r['url'])):
+        message += f"\nItem: {r['item'][i]}"
+        message += f"\nPreço: {r['price'][i]}"
+        message += f"\nUrl: {r['url'][i]}"
         message += f"--------------------------------"
 
     context = ssl.create_default_context()
@@ -61,31 +61,39 @@ def email_results(results):
     print(f"Email sent to {RECIPIENT_EMAIL}")
 
 
-for item in wishlist:
-    search_url = 'https://www.olx.pt/ads/q-' + item.replace(" ", "-")
-    r = requests.get(search_url)
-    root = lxml.html.fromstring(r.content)
+def criado():
+    for item in wishlist:
+        search_url = 'https://www.olx.pt/ads/q-' + item.replace(" ", "-")
+        r = requests.get(search_url)
+        root = lxml.html.fromstring(r.content)
 
-    ads = root.xpath("//td[contains(@class,'offer')]")
+        ads = root.xpath("//td[contains(@class,'offer')]")
 
-    for ad in ads:
-        url = ad.xpath(".//h3/a/@href")
-        if len(url) == 0:
-            continue
-        url = url[0]
-        title = ad.xpath(".//h3/a/strong/text()")[0]
-        price = ad.xpath(".//p[contains(@class,'price')]/strong/text()")[0].strip()
-        price = float(price.replace("€", ""))
+        for ad in ads:
+            url = ad.xpath(".//h3/a/@href")
+            if len(url) == 0:
+                continue
+            url = url[0]
+            title = ad.xpath(".//h3/a/strong/text()")[0]
+            price = ad.xpath(".//p[contains(@class,'price')]/strong/text()")[0].strip()
+            price = float(price.replace("€", ""))
 
-        if df['url'].str.contains(url).sum() == 0:
-            save_ad(results, item, url, title, price)
-        else:
-            if df[df['url'] == url]['price'].values[0] > price:
-                index = df[df['url'] == url].index
-                df.drop(index, axis=0, inplace=True)
+            if df['url'].str.contains(url).sum() == 0:
                 save_ad(results, item, url, title, price)
+            else:
+                if df[df['url'] == url]['price'].values[0] > price:
+                    index = df[df['url'] == url].index
+                    df.drop(index, axis=0, inplace=True)
+                    save_ad(results, item, url, title, price)
 
-df = pd.concat([df, pd.DataFrame(results)], axis=0)
-df.to_csv(DATA_FILE, index=False)
-email_results(results)
-print(f"Found {len(results['url'])} ads")
+    df2 = pd.concat([df, pd.DataFrame(results)], axis=0)
+    df2.to_csv(DATA_FILE, index=False)
+    email_results(results)
+    print(f"Found {len(results['url'])} ads")
+
+
+# Create an instance of scheduler and add function.
+scheduler = BlockingScheduler()
+scheduler.add_job(criado, "interval", seconds=60)
+
+scheduler.start()
